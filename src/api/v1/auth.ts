53f9db46f494edce5638070c9cd4ed7768616779
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-10-22 11:58:42 -0300

    Add authentication route

--- src/api/v1/auth.ts
+++ src/api/v1/auth.ts
@@ -45,4 +45,51 @@ router.post('/register', async (request, response) => {
   }
 });
 
+router.post('/authenticate', async (request, response) => {
+  const { username, password } = request.body;
+  const apiResponse = new ApiResponse();
+  if (!username) {
+    apiResponse.addError({
+      type: 'ValidationError',
+      kind: 'required',
+      message: 'Field `username` is required'
+    });
+  }
+
+  if (!password) {
+    apiResponse.addError({
+      type: 'ValidationError',
+      kind: 'required',
+      message: 'Field `password` is required'
+    });
+  }
+
+  if (!username || !password) {
+    return response.status(400).json(apiResponse.json());
+  }
+
+  const user = await User.findOne({ username }).select('+password');
+  if (!user) {
+    apiResponse.addError({
+      type: 'EntityNotFound',
+      message: `User \`${username}\` is not registered`,
+      kind: 'notfound'
+    });
+    return response.status(404).send(apiResponse.json());
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    apiResponse.addError({
+      type: 'InvalidPassword',
+      message: 'Wrong password',
+      kind: 'password'
+    });
+    return response.status(400).send(apiResponse.json());
+  }
+
+  user.password = undefined;
+
+  return response.status(201).json({ user, token: generateJwt({ id: user.id }) });
+});
+
 export default router;
