ed1746908460a0625ab173ea38e7b2fd10e17c68
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-10-24 15:32:18 -0300

    Add playlist model

---
+++ src/models/playlist.model.ts
@@ -0,0 +1,39 @@
+import mongoose from '../config/mongoose';
+
+export interface IPlaylist extends mongoose.Document {
+  guildId: string;
+  name: string;
+  creator: string;
+  allowModify: boolean;
+  createdAt: Date;
+}
+
+export const PlaylistSchema = new mongoose.Schema({
+  guild: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Guild',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: String,
+    required: true
+  },
+  allowModify: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+PlaylistSchema.index({ guild: 1, name: 1 }, { unique: true });
+
+export const Playlist = mongoose.model<IPlaylist>('Playlist', PlaylistSchema);
+
+export default Playlist;
