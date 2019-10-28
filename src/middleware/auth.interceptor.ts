0ab1dd0b7ec7f4d25c2f41fd8b83c1df5fe54802
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-10-28 14:46:37 -0300

    use jwt sync verify instead of callback

--- src/middleware/auth.interceptor.ts
+++ src/middleware/auth.interceptor.ts
@@ -3,12 +3,17 @@ import jwt from 'jsonwebtoken';
 import env from '../config/env';
 
 import ApiResponse from '../app/api.response';
+import Logger from '../app/api.logger';
 
 import errorTypes from '../app/types/errors';
 
-export default function(request: Request, response: Response, next: NextFunction) {
+export default function(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   const apiResponse = new ApiResponse();
-  const error = {
+  const errorResponse = {
     type: errorTypes.authentication.error.token,
     message: '',
     kind: 'authentication.error.token'
@@ -17,38 +22,38 @@ export default function(request: Request, response: Response, next: NextFunction
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    error.message = 'No token provided';
-    apiResponse.addError(error);
+    errorResponse.message = 'No token provided';
+    apiResponse.addError(errorResponse);
     return response.status(401).json(apiResponse.json());
   }
 
   const parts = authHeader.split(' ');
   if (parts.length !== 2) {
-    error.message = 'Invalid token';
-    apiResponse.addError(error);
+    errorResponse.message = 'Invalid token';
+    apiResponse.addError(errorResponse);
     return response.status(401).json(apiResponse.json());
   }
 
   const [scheme, token] = parts;
   if (!/^Bearer$/i.test(scheme)) {
-    error.message = 'Malformed token';
-    apiResponse.addError(error);
+    errorResponse.message = 'Malformed token';
+    apiResponse.addError(errorResponse);
     return response.status(401).json(apiResponse.json());
   }
 
-  jwt.verify(token, env.tokenSecret, (err, decoded) => {
-    if (err) {
-      error.message = 'Error on processing token. Invalid token';
-      apiResponse.addError(error);
-      return response.status(401).json(apiResponse.json());
-    };
-
+  try {
+    const decoded = jwt.verify(token, env.tokenSecret);
     if (typeof decoded === 'string') {
+      Logger.ApiConsole.normal(decoded);
       return;
     }
 
     request.userId = (decoded as any).id;
-  });
-
-  next();
+    next();
+  } catch (error) {
+    Logger.ApiConsole.error(error);
+    errorResponse.message = 'Error on processing token. Invalid token';
+    apiResponse.addError(errorResponse);
+    return response.status(401).json(apiResponse.json());
+  }
 }
