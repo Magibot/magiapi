a00e75a5ace38febaa89e84c4b382e0c9f887b2d
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-10-30 17:08:00 -0300

    add reset password route

--- src/api/v1/routes/auth.ts
+++ src/api/v1/routes/auth.ts
@@ -43,15 +43,15 @@ router.post('/register', async (request, response) => {
     });
 
     await user.generateAccessToken();
-    await user.generateTemporaryPassword();
+    const password = await user.generateTemporaryPassword();
     user = await user.save();
 
-    const { accessToken, password, passwordExpirationDate } = user;
+    const { accessToken, passwordExpirationDate } = user;
     apiResponse.setPayload({
       token: accessToken,
       tokenExpiresIn: env.tokenExpirationTime,
       password: password,
-      expirationDate: passwordExpirationDate,
+      expirationDate: passwordExpirationDate
     });
     return response.status(201).json(apiResponse.json());
   } catch (err) {
@@ -83,7 +83,9 @@ router.post('/authenticate', async (request, response) => {
     return response.status(400).json(apiResponse.json());
   }
 
-  const user = await User.findOne({ username }).select('+password +accessToken');
+  const user = await User.findOne({ username }).select(
+    '+password +accessToken'
+  );
   if (!user) {
     apiResponse.addError({
       type: errorTypes.entity.notfound,
@@ -106,52 +108,87 @@ router.post('/authenticate', async (request, response) => {
 
   await user.generateAccessToken();
   await user.save();
-  
+
   const { accessToken } = user;
   user.hideSensibleData();
   return response.status(201).json({ user, token: accessToken });
 });
 
-router.post('/logout', bearerAuthenticationInterceptor, async (request, response) => {
-  const user = await User.findById(request.userId).select('+accessToken');
-  if (user) {
-    await user.resetAccessToken();
+router.post(
+  '/logout',
+  bearerAuthenticationInterceptor,
+  async (request, response) => {
+    const user = await User.findById(request.userId).select('+accessToken');
+    if (user) {
+      await user.resetAccessToken();
+      await user.save();
+    }
+
+    return response.status(204).json();
+  }
+);
+
+router.post(
+  '/change-password',
+  bearerAuthenticationInterceptor,
+  async (request, response) => {
+    const { newPassword, confirmationPassword } = request.body;
+    const apiResponse = new ApiResponse();
+    if (newPassword !== confirmationPassword) {
+      apiResponse.addError({
+        type: errorTypes.authentication.password.confirmation,
+        message:
+          'Confirmation password does not match with the password provided',
+        kind: 'authentication.password.confirmation'
+      });
+
+      return response.status(400).json(apiResponse.json());
+    }
+
+    const user = await User.findById(request.userId).select('+password');
+    if (!user) {
+      apiResponse.addError({
+        type: errorTypes.authentication.error.token,
+        message: "Token provided reflects an user that don't exist",
+        kind: 'authentication.error.token'
+      });
+
+      return response.status(400).json(apiResponse.json());
+    }
+
+    user.password = newPassword;
+    user.isValidated = true;
+    user.passwordExpirationDate = null;
     await user.save();
+
+    return response.status(201).json();
   }
+);
 
-  return response.status(204).json();
-});
-
-router.post('/change-password', bearerAuthenticationInterceptor, async (request, response) => {
-  const { newPassword, confirmationPassword } = request.body;
+router.post('/reset-password', async (request, response) => {
+  const { username } = request.body;
   const apiResponse = new ApiResponse();
-  if (newPassword !== confirmationPassword) {
-    apiResponse.addError({
-      type: errorTypes.authentication.password.confirmation,
-      message: 'Confirmation password does not match with the password provided',
-      kind: 'authentication.password.confirmation'
-    });
-    
-    return response.status(400).json(apiResponse.json());
-  }
+  const user = await User.findOne({ username }).select(
+    '+password +expiredPassword'
+  );
 
-  const user = await User.findById(request.userId).select('+password');
   if (!user) {
     apiResponse.addError({
-      type: errorTypes.authentication.error.token,
-      message: 'Token provided reflects an user that don\'t exist',
-      kind: 'authentication.error.token'
+      type: errorTypes.entity.notfound,
+      message: `User \`${username}\` is not registered`,
+      kind: 'entity.notfound'
     });
-    
-    return response.status(400).json(apiResponse.json());
+
+    return response.status(404).send(apiResponse.json());
   }
 
-  user.password = newPassword;
-  user.isValidated = true;
-  user.passwordExpirationDate = null;
+  const password = await user.generateTemporaryPassword();
   await user.save();
-
-  return response.status(201).json();
+  apiResponse.setPayload({
+    password: password,
+    expirationDate: user.passwordExpirationDate
+  });
+  return response.status(201).json(apiResponse.json());
 });
 
 export default router;
