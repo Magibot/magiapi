1247dad6893c2daf90f7943f14f6ab13cbbd92ad
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-04 18:36:16 -0300

    Setup a swagger specification page

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 
 import auth from './routes/auth';
 import guild from './routes/guild';
 
 const router = express.Router({ mergeParams: true });
+
+// Require the swagger document
+const document = require('./swagger.json');
+router.use('/docs', swagger.serve, swagger.setup(document, { customCss: '.swagger-ui .topbar { display: none }' }));
 
 router.use('/auth', auth);
 router.use('/guilds', guild);
