fa6cd07931266ae1b9866406874d115edf5e3266
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-06 17:19:36 -0300

    Setup ci use variable

--- tests/properties.js
+++ tests/properties.js
@@ -1,7 +1,10 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 
-const baseUrl = `http://localhost:${process.env.PORT}`;
+const baseUrl =
+  process.env.NODE_ENV === 'ci'
+    ? 'http://magi-api.herokuapp.com/'
+    : `http://localhost:${process.env.PORT}`;
 const apiVersion = '/api/v1';
 
 module.exports = {
