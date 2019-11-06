cb9c38703e9cc037f0dd1469416a0584d87cb14f
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-06 16:41:30 -0300

    Set client id from env

--- tests/properties.js
+++ tests/properties.js
@@ -1,11 +1,10 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
-const env = require('./.test.env');
 
 const baseUrl = `http://localhost:${process.env.PORT}`;
 const apiVersion = '/api/v1';
 
 module.exports = {
   host: `${baseUrl}${apiVersion}`,
-  clientId: env.clientId
+  clientId: process.env.TEST_CLIENT_ID
 };
