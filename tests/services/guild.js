5b6f0e00553d3f7b0aca40f1ec323a3408a08b29
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-12-02 11:53:14 -0300

    Add guild services for get, put, patch and delete

--- tests/services/guild.js
+++ tests/services/guild.js
@@ -15,4 +15,62 @@ const create = async guild => {
   return response;
 };
 
-module.exports = { create };
+const byId = async (id, type) => {
+  const response = await chai
+    .request(properties.host)
+    .get(`/guilds/${id}`)
+    .query({ typeId: type })
+    .set({
+      Authorization: properties.clientId
+    })
+    .send()
+    .catch(err => console.error(err));
+
+  return response;
+};
+
+const update = async (id, type, guild) => {
+  const response = await chai
+    .request(properties.host)
+    .put(`/guilds/${id}`)
+    .query({ typeId: type })
+    .set({
+      Authorization: properties.clientId,
+      'Content-Type': 'application/json'
+    })
+    .send(guild)
+    .catch(err => console.error(err));
+
+  return response;
+};
+
+const edit = async (id, type, document) => {
+  const response = await chai
+    .request(properties.host)
+    .patch(`/guilds/${id}`)
+    .query({ typeId: type })
+    .set({
+      Authorization: properties.clientId,
+      'Content-Type': 'application/json'
+    })
+    .send(document)
+    .catch(err => console.error(err));
+
+  return response;
+};
+
+const destroy = async (id, type) => {
+  const response = await chai
+    .request(properties.host)
+    .delete(`/guilds/${id}`)
+    .query({ typeId: type })
+    .set({
+      Authorization: properties.clientId
+    })
+    .send()
+    .catch(err => console.error(err));
+
+  return response;
+};
+
+module.exports = { create, byId, update, edit, destroy };
