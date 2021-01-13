ea0aa5234331ec033b0fbd81c8d77a0f820fb164
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-13 18:17:09 -0300

    Remove lombok configs on entites

--- PlaylistManager/src/main/java/com/magi/playlistmanager/domain/core/guild/DiscordServer.java
+++ PlaylistManager/src/main/java/com/magi/playlistmanager/domain/core/guild/DiscordServer.java
@@ -5,5 +5,23 @@ public class DiscordServer {
     private final String idFromDiscord;
     private final String region;
     private final String owner;
+
+    public DiscordServer(String idFromDiscord, String region, String owner) {
+        this.idFromDiscord = idFromDiscord;
+        this.region = region;
+        this.owner = owner;
+    }
+
+    public String getIdFromDiscord() {
+        return idFromDiscord;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
     
 }
--- PlaylistManager/src/main/java/com/magi/playlistmanager/domain/core/guild/Guild.java
+++ PlaylistManager/src/main/java/com/magi/playlistmanager/domain/core/guild/Guild.java
@@ -1,19 +1,30 @@
 package com.magi.playlistmanager.domain.core.guild;
 
-import com.magi.playlistmanager.domain.core.ApplicationDate;
 import com.magi.playlistmanager.domain.core.Entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
 public class Guild extends Entity {
     
     private final String name;
     private final String iconHash;
     private final DiscordServer discordServer;
+
+    public Guild(String name, String iconHash, DiscordServer discordServer) {
+        super();
+        this.name = name;
+        this.iconHash = iconHash;
+        this.discordServer = discordServer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconHash() {
+        return iconHash;
+    }
+
+    public DiscordServer getDiscordServer() {
+        return discordServer;
+    }
 
 }
