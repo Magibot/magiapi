bb697074247524abcefce3aa9bdc8dccd47d8db2
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-13 18:18:48 -0300

    Add guild controller to expose on http rest

---
+++ PlaylistManager/src/main/java/com/magi/playlistmanager/infrastructure/inbound/http/controllers/GuildsController.java
@@ -0,0 +1,30 @@
+package com.magi.playlistmanager.infrastructure.inbound.http.controllers;
+
+import com.magi.playlistmanager.domain.application.guild.GuildApplicationService;
+import com.magi.playlistmanager.domain.application.guild.dto.GuildDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("guilds")
+public class GuildsController {
+
+    private final GuildApplicationService guildApplicationService;
+
+    @Autowired
+    public GuildsController(final GuildApplicationService guildApplicationService) {
+        this.guildApplicationService = guildApplicationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GuildDto> postGuild(@RequestBody GuildDto guildDto) {
+        GuildDto guildCreated = guildApplicationService.createGuild(guildDto);
+        return ResponseEntity.status(201).body(guildCreated);
+    }
+    
+}
