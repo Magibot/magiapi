9650d04718bf74fcc94e6d481634797eb011a876
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-18 14:49:56 -0300

    Add create playlist method on guild application service

--- src/main/java/com/magi/manager/domain/application/guild/GuildApplicationService.java
+++ src/main/java/com/magi/manager/domain/application/guild/GuildApplicationService.java
@@ -1,9 +1,12 @@
 package com.magi.manager.domain.application.guild;
 
 import com.magi.manager.domain.application.guild.dto.GuildDto;
+import com.magi.manager.domain.application.playlist.dto.PlaylistDto;
 
 public interface GuildApplicationService {
     
     GuildDto createGuild(GuildDto guildDto);
+
+    PlaylistDto createPlaylist(String guildId, PlaylistDto playlistDto);
 
 }
--- src/main/java/com/magi/manager/domain/application/guild/GuildApplicationServiceImpl.java
+++ src/main/java/com/magi/manager/domain/application/guild/GuildApplicationServiceImpl.java
@@ -1,15 +1,24 @@
 package com.magi.manager.domain.application.guild;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.magi.manager.domain.application.guild.dto.DiscordServerDto;
 import com.magi.manager.domain.application.guild.dto.GuildDto;
+import com.magi.manager.domain.application.playlist.dto.PlaylistDto;
+import com.magi.manager.domain.core.guild.CreateGuildFactory;
 import com.magi.manager.domain.core.guild.DiscordServer;
 import com.magi.manager.domain.core.guild.Guild;
+import com.magi.manager.domain.core.playlist.Playlist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GuildApplicationServiceImpl implements GuildApplicationService {
+
+    @Autowired
+    private CreateGuildFactory createGuildFactory;
 
     private final GuildRepository guildRepository;
 
@@ -18,27 +27,52 @@ public class GuildApplicationServiceImpl implements GuildApplicationService {
         this.guildRepository = guildRepository;
     }
 
+    private Playlist toPlaylist(PlaylistDto playlistDto) {
+        return new Playlist(
+            playlistDto.getId(),
+            playlistDto.getName(),
+            playlistDto.getGuildId(),
+            playlistDto.getCreator(),
+            playlistDto.getCreationDate());
+    } 
+
     private DiscordServer toDiscordServer(DiscordServerDto discordServerDto) {
-        return new DiscordServer(
-            discordServerDto.getIdFromDiscord(),
-            discordServerDto.getRegion(),
-            discordServerDto.getOwner());
+        return new DiscordServer(discordServerDto.getIdFromDiscord(), discordServerDto.getRegion(),
+                discordServerDto.getOwner());
     }
 
     private Guild toGuild(GuildDto guildDto) {
+        List<Playlist> playlists = new ArrayList<>();
+        guildDto.getPlaylists().forEach(playlistDto -> playlists.add(toPlaylist(playlistDto)));
         DiscordServer discordServer = this.toDiscordServer(guildDto.getDiscordServer());
         return new Guild(
-            guildDto.getName(), 
-            guildDto.getIconHash(), 
-            discordServer);
+            guildDto.getId(),
+            guildDto.getName(),
+            guildDto.getIconHash(),
+            discordServer, 
+            playlists, 
+            guildDto.getCreationDate());
     }
 
     @Override
     public GuildDto createGuild(GuildDto guildDto) {
-        Guild guild = toGuild(guildDto);
+        Guild guild = createGuildFactory.execute(
+            guildDto.getName(), 
+            guildDto.getIconHash(), 
+            toDiscordServer(guildDto.getDiscordServer()));
         GuildDto guildCreated = GuildDto.from(guild);
         this.guildRepository.save(guildCreated);
         return guildCreated;
+    }
+
+    @Override
+    public PlaylistDto createPlaylist(String guildId, PlaylistDto playlistDto) {
+        GuildDto guildDto = guildRepository.findById(guildId);
+        Guild guild = toGuild(guildDto);
+        Playlist playlist = guild.createPlaylist(playlistDto.getName(), playlistDto.getCreator());
+        PlaylistDto playlistCreated = PlaylistDto.from(playlist);
+        guildRepository.addPlaylist(guildId, playlistCreated);;
+        return playlistCreated;
     }
     
 }
