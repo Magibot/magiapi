f641fb4f92a1e4d26428e009628bd1de53e81ba4
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-18 16:23:48 -0300

    Implement operations on controller to find guild by id and create playlist on them

--- src/main/java/com/magi/manager/infrastructure/inbound/http/controllers/GuildsController.java
+++ src/main/java/com/magi/manager/infrastructure/inbound/http/controllers/GuildsController.java
@@ -2,9 +2,12 @@ package com.magi.manager.infrastructure.inbound.http.controllers;
 
 import com.magi.manager.domain.application.guild.GuildApplicationService;
 import com.magi.manager.domain.application.guild.dto.GuildDto;
+import com.magi.manager.domain.application.playlist.dto.PlaylistDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,18 @@ public class GuildsController {
     public ResponseEntity<GuildDto> postGuild(@RequestBody GuildDto guildDto) {
         GuildDto guildCreated = guildApplicationService.createGuild(guildDto);
         return ResponseEntity.status(201).body(guildCreated);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<GuildDto> getGuildById(@PathVariable String id) {
+        GuildDto guild = guildApplicationService.getGuild(id);
+        return ResponseEntity.status(200).body(guild);
+    }
+
+    @PostMapping("{id}/playlists")
+    public ResponseEntity<PlaylistDto> postPlaylist(@PathVariable String id, @RequestBody PlaylistDto playlistDto) {
+        PlaylistDto playlist = guildApplicationService.createPlaylist(id, playlistDto);
+        return ResponseEntity.status(201).body(playlist);
     }
     
 }
