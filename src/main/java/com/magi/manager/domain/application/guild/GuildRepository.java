6e0568e743dfae1f71ab21f8ed767d9f2398bf92
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-18 14:50:20 -0300

    Add methods do retrive guild data and add playlists on repository

--- src/main/java/com/magi/manager/domain/application/guild/GuildRepository.java
+++ src/main/java/com/magi/manager/domain/application/guild/GuildRepository.java
@@ -1,9 +1,14 @@
 package com.magi.manager.domain.application.guild;
 
 import com.magi.manager.domain.application.guild.dto.GuildDto;
+import com.magi.manager.domain.application.playlist.dto.PlaylistDto;
 
 public interface GuildRepository {
     
     void save(GuildDto guildDto);
+
+    void addPlaylist(String guildId, PlaylistDto playlistDto);
+
+    GuildDto findById(String guildId);
 
 }
