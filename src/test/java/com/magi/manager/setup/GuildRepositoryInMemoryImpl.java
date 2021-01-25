ca0a4e1d669232a66172baaede30abbe76c858db
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-01-25 16:36:24 -0300

    Add unimplemented method to test repository

--- src/test/java/com/magi/manager/setup/GuildRepositoryInMemoryImpl.java
+++ src/test/java/com/magi/manager/setup/GuildRepositoryInMemoryImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import com.magi.manager.domain.application.guild.GuildRepository;
 import com.magi.manager.domain.application.guild.dto.GuildDto;
+import com.magi.manager.domain.application.member.MemberDto;
 import com.magi.manager.domain.application.playlist.dto.PlaylistDto;
+import com.magi.manager.domain.exception.GuildNotFoundException;
 
 public class GuildRepositoryInMemoryImpl implements GuildRepository {
 
@@ -26,6 +28,12 @@ public class GuildRepositoryInMemoryImpl implements GuildRepository {
     public GuildDto findById(String guildId) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public void addMember(String guildId, MemberDto memberDto) throws GuildNotFoundException {
+        // TODO Auto-generated method stub
+
     }
     
 }
