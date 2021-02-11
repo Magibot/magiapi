4738d812940c534b632d513eb80991c47bcd6ae5
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-11 18:04:53 -0300

    Remove public modifier

--- src/test/java/com/venuses/manager/MemberTests.java
+++ src/test/java/com/venuses/manager/MemberTests.java
@@ -14,7 +14,7 @@ import com.venuses.manager.domain.exception.MemberDuplicatedException;
 
 import org.junit.jupiter.api.Test;
 
-public class MemberTests {
+class MemberTests {
 
 	private final CreateGuildFactory createGuildFactory = new CreateGuildFactory();
 
