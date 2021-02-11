33fdf4453bb0f4424f9103b52065216a887eaec6
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-11 13:51:07 -0300

    Add test that validates member duplicated exception caused by idFromDiscord

--- src/test/java/com/venuses/manager/ManagerApplicationTests.java
+++ src/test/java/com/venuses/manager/ManagerApplicationTests.java
@@ -1,7 +1,9 @@
 package com.venuses.manager;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +31,8 @@ class ManagerApplicationTests {
 
 	private List<GuildDto> guilds = new ArrayList<>();
 	private List<MemberDto> members = new ArrayList<>();
+
+	private GuildDto guild;
 
 	private GuildRepository guildRepository = new GuildRepositoryInMemoryImpl(guilds, members);
 	private MemberRepository memberRepository = new MemberRepositoryInMemoryImpl(members);
@@ -60,6 +64,35 @@ class ManagerApplicationTests {
 
 		guild = this.guildApplicationService.getGuild(guild.getId());
 		assertTrue(guild.getMembers().contains(member));
+	}
+
+	@Test
+	void shouldTryToAddDuplicatedMemberToGuild() throws GuildNotFoundException, MemberDuplicatedException {
+		GuildDto guildRequest = new GuildDto("Python Community", "1234567",
+		new DiscordServerDto("2", "Ireland", "Matthew"));
+		guild = this.guildApplicationService.createGuild(guildRequest);
+		assertNotNull(guild.getId());
+
+		MemberDto memberRequest = new MemberDto("7356719871", "mellomaths", true);
+		try {
+			MemberDto member = this.guildApplicationService.addMember(guild.getId(), memberRequest);
+			assertNotNull(member.getId());
+			assertNotNull(member.getCreationDate());
+
+			guild = this.guildApplicationService.getGuild(guild.getId());
+			assertTrue(guild.getMembers().contains(member));
+
+			memberRequest = new MemberDto("7356719871", "newman", true);
+			MemberDuplicatedException exception = assertThrows(MemberDuplicatedException.class, () -> {
+				MemberDto memberDto = new MemberDto("7356719871", "newman", true);
+				this.guildApplicationService.addMember(guild.getId(), memberDto);
+			});
+
+			assertTrue(exception.getMessage().contains("idFromDiscord=7356719871"));
+			assertTrue(exception.getMessage().contains("was already added to Guild"));
+		} catch (MemberDuplicatedException ex) {
+			fail("Duplicated member could not be generated! Check the testing database.");
+		}
 	}
 
 	@Test
