a594bd85db8172d602586e2f038e2a39ab2caf00
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-02-20 02:39:39 -0300

    Add should not add song to closed playlist test

--- src/test/java/com/venuses/manager/PlaylistTests.java
+++ src/test/java/com/venuses/manager/PlaylistTests.java
@@ -2,6 +2,8 @@ package com.venuses.manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.venuses.manager.domain.core.playlist.Playlist;
 import com.venuses.manager.domain.core.song.Song;
@@ -44,6 +46,50 @@ class PlaylistTests {
 		assertEquals(songArtist, songCreated.getArtist());
 		assertEquals(songYoutubeLink, songCreated.getYoutubeLink());
 		assertEquals(playlist.getCreator().toString(), songCreated.getAddedBy().toString());
+	}
+
+	@Test
+	void shouldAddSongToAnotherMemberOpenPlaylists() throws PlaylistNotOpenException {
+		String guildId = "123456789";
+		String creator = "28761782";
+
+		Playlist playlist = new Playlist("Rock Playlist", guildId, creator, true);
+		assertNotNull(playlist.getId());
+		assertEquals(0, playlist.getSongs().size());
+
+		String songName = "Blacking out";
+		String songArtist = "Lil Wayne";
+		String songYoutubeLink = "http://youtube.com/12878192";
+		String anotherMember = "272532212";
+		playlist.addSong(songName, songArtist, anotherMember, songYoutubeLink);
+		assertEquals(1, playlist.getSongs().size());
+
+		Song songCreated = playlist.getSongs().get(0);
+		assertNotNull(songCreated.getId());
+		assertEquals(songName, songCreated.getName());
+		assertEquals(songArtist, songCreated.getArtist());
+		assertEquals(songYoutubeLink, songCreated.getYoutubeLink());
+	}
+
+	@Test
+	void shouldNotAddSongToClosedPlaylist() {
+		String guildId = "123456789";
+		String creator = "28761782";
+
+		Playlist playlist = new Playlist("Rock Playlist", guildId, creator, false);
+		assertNotNull(playlist.getId());
+		assertEquals(0, playlist.getSongs().size());
+
+		String songName = "Blacking out";
+		String songArtist = "Lil Wayne";
+		String songYoutubeLink = "http://youtube.com/12878192";
+		String anotherMember = "272532212";
+		PlaylistNotOpenException exception = assertThrows(
+			PlaylistNotOpenException.class, 
+			() -> playlist.addSong(songName, songArtist, anotherMember, songYoutubeLink));
+		
+		assertEquals(0, playlist.getSongs().size());
+		assertTrue(exception.getMessage().contains("does not have permission to add songs to this playlist"));
 	}
 
 }
